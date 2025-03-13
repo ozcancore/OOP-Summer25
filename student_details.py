a18246ac0e76592640a647830c294bc300adf9b6
ozcancore <77274524+ozcancore@users.noreply.github.com>
2025-03-13 14:34:10 +0100

    Update student_details.py

--- student_details.py
+++ student_details.py
@@ -1,6 +1,6 @@
-first_name = "Alice"
-last_name = "Smith"
-index_number = "123456"
-nationality = "American"
+first_name = "Ozcan"
+last_name = "Yaylagul"
+index_number = "34765"
+nationality = "TUR"
 starting_date = datetime.date(2025, 03, 13)
 courses = ["Python Programming", "Data Structures", "Web Development"]
