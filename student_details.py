e73e966a0ea930de61e71c35e894c1a8495ec2a1
ozcancore <77274524+ozcancore@users.noreply.github.com>
2025-03-13 14:28:37 +0100

    Create s

---
+++ student_details.py
@@ -0,0 +1,6 @@
+first_name = "Alice"
+last_name = "Smith"
+index_number = "123456"
+nationality = "American"
+starting_date = datetime.date(2025, 03, 13)
+courses = ["Python Programming", "Data Structures", "Web Development"]
