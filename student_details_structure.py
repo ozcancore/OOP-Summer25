17b17e81fe00380796de3a96ff27851180b9cbce
ozcancore <77274524+ozcancore@users.noreply.github.com>
2025-03-13 15:04:59 +0100

    Update student_details_structure.py

--- student_details_structure.py
+++ student_details_structure.py
@@ -1,6 +1,28 @@
 student1 = {
- 'first_name':'OZCAN',
-  'last_name':'YAYLAGUL'
+ 'first_name':'Tommy',
+  'last_name':'Doe'
 }
+print(student1["first_name"])
 
-print(student1.first_name)
+student2 = {
+ 'first_name':'Tommy',
+  'last_name':'Josh'
+}
+print(student2["first_name"])
+
+
+student3 = {
+ 'first_name':'Jack',
+  'last_name':'Doe'
+}
+print(student3["first_name"])
+
+
+student4 = {
+ 'first_name':'Kray',
+  'last_name':'Six'
+}
+print(student4["first_name"])
+
+
+
